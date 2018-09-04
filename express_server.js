e8d73a03791da594741d9ca6974378a5104ac056
Fei Gao <fgao1@ualberta.ca>
2018-09-04 14:55:33 -0600

    Add a POST route to receive form submission

--- express_server.js
+++ express_server.js
@@ -1,13 +1,13 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-var PORT = 8080; // default port 8080
+const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -28,6 +28,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // debug statement to see POST parameters
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
