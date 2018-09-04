945643ad304847f1825d23e30df73f3f9c5d6009
Fei Gao <fgao1@ualberta.ca>
2018-09-04 14:44:29 -0600

    Add a GET Route to Show the Form

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 var PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
+
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,6 +24,10 @@ app.get("/urls.json", (req, res) => {
 app.get("/urls", (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
