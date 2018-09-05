120642845c9217c0656e0c7cfada2b4438417082
Fei Gao <fgao1@ualberta.ca>
2018-09-04 22:14:01 -0600

    change variable names

--- express_server.js
+++ express_server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 const generateRandomString = require('./index.js');
 
 const PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,15 +30,17 @@ app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
 
+// POST
 // req.body returns {"longURL": https://wwww.google.ca}
 // after submiting form, save shortURL and longURL to database
 // and redirect user to /urls/<shortURL> page 
 app.post("/urls", (req, res) => {
     let random = generateRandomString();
-    urlDatabase[random] = req.body["longURL"];  
+    urlDatabase[random] = req.body.longURL;  
     res.redirect(`/urls/${random}`);
 });
 
+// Show
 app.get("/urls/:id", (req, res) => {
     let templateVars = { 
         shortURL: req.params.id,
@@ -47,22 +49,25 @@ app.get("/urls/:id", (req, res) => {
     res.render("urls_show", templateVars);
 });
 
+//Redirect
 app.get("/u/:shortURL", (req, res) => {
-    let urlKey = req.params.shortURL;
-    let longURL = urlDatabase[urlKey];
+    let shortURL = req.params.shortURL;
+    let longURL = urlDatabase[shortURL];
     res.redirect(longURL);
 });
 
 //Delete form
 app.post("/urls/:id/delete", (req, res) => {
-    delete urlDatabase[req.params.id];
+    const shortURL = req.params.id;
+    delete urlDatabase[shortURL];
     res.redirect("/urls");
 });
 
-//Update longURL
+//Update 
 app.post("/urls/:id", (req, res) => {
     const shortURL = req.params.id;
-    urlDatabase[shortURL] = req.body["longURL"];
+    const longURL = req.body.longURL;
+    urlDatabase[shortURL] = longURL;
     res.redirect("/urls");
 });
 
